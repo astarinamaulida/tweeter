3f3679100fa42d89e573ebc6c285e0635bcd3490
Astarina Maulida <astarina.maulida@gmail.com>
2021-10-06 05:28:28 -0400

    Add red class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,5 +1,16 @@
 
-$(document).ready(function() {
+$(document).ready(function (event) {
   // --- our code goes here ---
+  $('.new-tweet textarea').on('input', function() {
+    let newTweetLength = $(this).val().length;
+    let nearbyCounter = $(this).parent().find('.counter');
+    const tweetLengthLimit = 140;
 
+    if (newTweetLength > tweetLengthLimit) {
+      nearbyCounter.addClass('red');
+    } else if (newTweetLength <= tweetLengthLimit) {
+      nearbyCounter.removeClass('red');
+    }
+    nearbyCounter.text(tweetLengthLimit - newTweetLength);
+  });
 });
