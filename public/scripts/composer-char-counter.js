bc0390d4a6b745f135359c3d4826aac8f23cc7bf
Astarina Maulida <astarina.maulida@gmail.com>
2021-10-06 06:07:41 -0400

    Add all tweets class

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function (event) {
   // --- our code goes here ---
   $('.new-tweet textarea').on('input', function() {
@@ -6,6 +5,7 @@ $(document).ready(function (event) {
     let nearbyCounter = $(this).parent().find('.counter');
     const tweetLengthLimit = 140;
 
+    /* Add class red if the new tweet is longer than tweet length limit (140) */
     if (newTweetLength > tweetLengthLimit) {
       nearbyCounter.addClass('red');
     } else if (newTweetLength <= tweetLengthLimit) {
