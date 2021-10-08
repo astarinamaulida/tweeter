b5914fddbc0e1959318385e485b126f9da9381b7
Astarina Maulida <astarina.maulida@gmail.com>
2021-10-08 06:07:25 -0400

    Fix lints and clean code

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,105 +4,90 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// Short format of jQuery 
+// Short format of jQuery
 $(() => {
 
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
 
-  // Loops through tweets
-  // Calls createTweetElement for each tweet
-  // Takes return value and appends it to the tweets container
-  // Append to tweets container. 
   // Prepend gets the latest post first
-  const renderTweets = function (tweets) {
+  const renderTweets = function(tweets) {
     for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet)
-      $('section.all-tweets').prepend($tweet);
+      const $tweet = createTweetElement(tweet);
+      $("section.all-tweets").prepend($tweet);
     }
-  }
+  };
 
   // Get the article of tweet
-  // Moved out from HTML file 
-  const createTweetElement = function (tweet) {
+  const createTweetElement = function(tweet) {
     const $tweet = `
     <article class="tweet">
-  <header class="tweet header">
-    <div class="user-profile">
-      <img id="icon" src=${tweet.user.avatars}/>
-      <p class="name">${tweet.user.name}</p>
-    </div>
-      <span class="username"> ${tweet.user.handle} </span>
-  </header>
-  <p class= "tweet-text">${escape(tweet.content.text)}</p>
-  <footer>
-    <div class="date">${timeago.format(tweet.created_at)}</div>
-    <div class = "icons">
-      <i class="fa-solid fa-retweet"></i>
-      <i class="fa-solid fa-flag"></i>
-      <i class="fa-solid fa-heart"></i>
-    </div>
-  </footer>
-  </article>`;
+      <header class="tweet header">
+       <div class="user-profile">
+         <img id="icon" src=${tweet.user.avatars}/>
+         <p class="name">${tweet.user.name}</p>
+        </div>
+        <span class="username"> ${tweet.user.handle} </span>
+      </header>
+      <p class= "tweet-text">${escape(tweet.content.text)}</p>
+      <footer>
+        <div class="date">${timeago.format(tweet.created_at)}</div>
+        <div class = "icons">
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>`;
     return $tweet;
-  }
+  };
 
 
   // Make GET request to tweet database
   const loadTweets = function () {
-    //$.ajax('/tweets', { method: "GET" })
-    $.ajax('/tweets')
-      .then(function (allTweets) {
+    $.ajax("/tweets")
+      .then(function(allTweets) {
         renderTweets(allTweets);
       })
   }
   loadTweets();
 
-
   // Add an event listener that listens for the submit event
   // Prevent the default behaviour of the submit event (data submission and page refresh)
-  //$('.new-tweet form').submit(function (event) {
-    $('.new-tweet form').on("submit",function (event) {
+    $(".new-tweet form").on("submit",function (event) {
     event.preventDefault();
     const $form = $(this);
-    const newTweetTextStr = $form.children('textarea').val();
+    const newTweetTextStr = $form.children("textarea").val();
 
-    // If there is no input, error message pop up
-    // The new-tweet will slide down
+    // If there is no input, error message will slide up
     if (!newTweetTextStr) {
       $(".validation-text").slideUp();
-      $(".validation-text").html("All tweets must contain at least one character. Your tweet currently does not.").addClass("error-message").slideDown(600)
+      $(".validation-text").html("All tweets must contain at least one character. Your tweet currently does not.").addClass("error-message").slideDown(600);
       
-      // If the character is more than 140, error message pop up
-      // The new-tweet will slide down
+      // If the character is more than 140, error message will slide up
     } else if (newTweetTextStr.length > 140) {
       $(".validation-text").slideUp();
-      $(".validation-text").html("We do not accept tweets longer than 140 characters. Your tweet is currently too long.").addClass("error-message").slideDown(100)
+      $(".validation-text").html("We do not accept tweets longer than 140 characters. Your tweet is currently too long.").addClass("error-message").slideDown(600);
 
       // Serialize the form data and send it to the server as a query string
     } else {
       $(".validation-text").slideUp();
       const tweet = $form.serialize();
-      $.ajax({ url: "/tweets", method: 'POST', data: tweet })
-        //.then(function (successfulPost) {
-        //  return $.ajax('/tweets/', { method: 'GET' })
-        //})
-
-        // Reset the form and use renderTweets function to add new tweet to the page
-        .then(function () {
-          //$form[0].reset();
-          //$form.children('textarea').text(140);
-          $('counter').val(140);
-          $('.tweet-post').val('')
+      $.ajax({ url: "/tweets", method: "POST", data: tweet })
+        // Reset the form after new tweet published
+        // Counter return to 140 and tweet form back to empty
+        .then(function() {
+          $(".counter").text(140);
+          $(".tweet-post").val("")
           loadTweets();
         })
         .fail(function (err) {
           alert(err.responseJSON.error);
-        })
+        });
     }
-  })
+  });
   loadTweets();
 });
